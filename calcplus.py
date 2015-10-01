8fc4511cb3447f7f1359542fabceba97966be43e
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-01 23:40:41 +0200

    mejora de calcplus.py

--- calcplus.py
+++ calcplus.py
@@ -14,34 +14,27 @@ if __name__ == '__main__':
     for linea in lineas:
 	    datos = linea.split(',')
 	    operacion = datos[0]
-	    lenght = len(datos)
-	    for i in range(lenght):
-	       if i == 0:
-	           pass
-	       elif i == 1:
-	           operador1 = datos[i]
-	           operador2 = datos[i+1]
-	           result = calcoohija.operaciones(operacion, operador1, operador2, calc)
-	       elif i != lenght:
-	           if i == (lenght - 1):
-	               operador1 = result
-	               operador2 = datos[i+1][:-1]
-	               result = calcoohija.operaciones(operacion, operador1, operador2, calc)
-	           else:
-	               operador1 = result
-	               operador2 = datos[i+1]
-	               result = calcoohija.operaciones(operacion, operador1, operador2, calc)
+	    operandos = datos[1:]
+	    operandos[-1] = operandos[-1][:-1]
+	    print(operandos)
+	    result = int(operandos[0])
+        if operacion == "suma":
+            for operando in operandos[1:]:
+                operando = int(operando)
+                result = calc.suma(result, operando)
+        elif operacion == "resta":
+            for operando in operandos[1:]:
+                operando = int(operando)
+                result = calc.resta(result, operando)
+        elif operacion == "multi":
+            for operando in operandos[1:]:
+                operando = int(operando)
+                result = calc.multi(result, operando)
+        elif operacion == "div":
+            for operando in operandos[1:]:
+                operando = int(operando)
+                result = calc.div(result, operando)
+        else:
+            sys.exit('Operación sólo puede ser sumar, restar, multip o dividir.')
 
     print(result)
-    """if operacion == "suma":
-        result = calc.suma(operando1, operando2)
-    elif operacion == "resta":
-        result = calc.resta(operando1, operando2)
-    elif operacion == "multiplica":
-        result = calc.multi(operando1, operando2)
-    elif operacion == "divide":
-        result = calc.div(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar, restar, multip o dividir.')
-
-    print(result)"""
