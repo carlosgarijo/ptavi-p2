3cb0becc9f22be98527ef2607374fe983fcd1590
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-10-05 15:58:50 +0200

    calcplusplus.py mejorado importando calcplus.py

--- calcplusplus.py
+++ calcplusplus.py
@@ -4,10 +4,12 @@
 import sys
 import csv
 import calcoohija
+import calcplus
 
 if __name__ == '__main__':
 
     calc = calcoohija.CalculadoraHija()
+    dicc = {"suma": calc.suma, "resta": calc.resta, "multiplica": calc.multi, "divide": calc.div}
     fich = sys.argv[1]
     with open(fich, newline='') as fichero:
         lineas = csv.reader(fichero)
@@ -18,27 +20,8 @@ if __name__ == '__main__':
             """Cogemos los operandos"""
             operandos = linea[1:]
             result = int(operandos[0])
-            if operacion == "suma":
-                for operando in operandos[1:]:
-                    operando = int(operando)
-                    result = calc.suma(result, operando)
-                print(operacion + ": ", result)
-            elif operacion == "resta":
-                for operando in operandos[1:]:
-                    operando = int(operando)
-                    result = calc.resta(result, operando)
-                print(operacion + ": ", result)
-            elif operacion == "multiplica":
-                for operando in operandos[1:]:
-                    operando = int(operando)
-                    result = calc.multi(result, operando)
-                print(operacion + ": ", result)
-            elif operacion == "divide":
-                for operando in operandos[1:]:
-                    operando = int(operando)
-                    result = calc.div(result, operando)
-                print(operacion + ": ", result)
-            else:
-                sys.exit('Operacion only: sumar, restar, multip o dividir')
 
+            result = calcplus.operaciones(dicc, operacion, operandos, result)
+
+            print(operacion + ": ", result)
     # Cerramos el fichero
